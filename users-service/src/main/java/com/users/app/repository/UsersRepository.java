52ca58dd5b9b0687f70e6d4de7816ced2031e680
zetwenty <fadhillahzainum@gmail.com>
2025-03-09 12:20:49 +0700

    adding file UsersRepository. repository -> doneee

--- users-service/src/main/java/com/users/app/repository/UsersRepository.java
+++ users-service/src/main/java/com/users/app/repository/UsersRepository.java
@@ -1,5 +1,18 @@
 package com.users.app.repository;
 
-public interface UsersRepository {
+import com.users.app.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByEmail(String email);
+
+    Optional<Users> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
